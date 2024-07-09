a82c6dea28477e8cbc5bedc85619f45dc813ba7d
Israel dos Santos Candeias <kojimaster33@gmail.com>
2024-07-08 22:29:44 -0300

    pybuilder

--- build.py
+++ build.py
@@ -1,18 +1,28 @@
-# #   -*- coding: utf-8 -*-
-# from pybuilder.core import use_plugin, init
+#   -*- coding: utf-8 -*-
+from pybuilder.core import use_plugin, init
 
-# use_plugin("python.core")
-# use_plugin("python.unittest")
-# use_plugin("python.flake8")
-# use_plugin("python.coverage")
-# use_plugin("python.distutils")
+# These are the plugins we want to use in our project.
+# Projects provide tasks which are blocks of logic executed by PyBuilder.
 
-# name = "NCD-Playspace"
-# default_task = "publish"
+use_plugin("python.core")
+# the python unittest plugin allows running python's standard library unittests
+use_plugin("python.unittest")
+# this plugin allows installing project dependencies with pip
+use_plugin("python.install_dependencies")
+# a linter plugin that runs flake8 (pyflakes + pep8) on our project sources
+use_plugin("python.flake8")
+# a plugin that measures unit test statement coverage
+use_plugin("python.coverage")
+# for packaging purposes since we'll build a tarball
+use_plugin("python.distutils")
+
+name = "ncd_playspace"
+default_task = "publish"
 
 
 
-# @init
-# def set_properties(project):
-#     pass
+@init
+def set_properties(project):
+    project.set_property("coverage_break_build", False) # default is True
+    project.build_depends_on("mock")
 
